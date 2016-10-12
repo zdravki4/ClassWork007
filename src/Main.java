3ade816d23d638bcb68e463d7995e2765fc39ea5
zdravki4 <zdravko.gospodinov@gmail.com>
2016-10-12 20:18:41 +0300

    ClassWork 007 Task01

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 6;
+		sumTwoNumbers(num1, num2);
+	}
+	
+	public static void sumTwoNumbers(int num1, int num2){
+		int sumResult = num1 + num2;
+		System.out.println(sumResult);
+		
+	}
+
+}
