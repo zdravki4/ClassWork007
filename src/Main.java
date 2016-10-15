2c2c89bbfbf86c3b77858d41899644095102b5f1
zdravki4 <zdravko.gospodinov@gmail.com>
2016-10-15 14:22:59 +0300

    All tasks of ClassWork007

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
+import java.util.Arrays;
 public class Main {
-
 	public static void main(String[] args) {
-		int num1 = 5;
-		int num2 = 6;
-		sumTwoNumbers(num1, num2);
-	}
+		int a = 5;
+		int b = 6;
+		sum(a, b);
+		int sumata = sumReturn(a, b);
+		System.out.println("Sumata e: " + sumata);
 	
-	public static void sumTwoNumbers(int num1, int num2){
-		int sumResult = num1 + num2;
-		System.out.println(sumResult);
-		
+		int[] array = new int[] { 1, 2, 3, 4, 5 };
+		backOrder(array);
+		String test = "qwerty";
+		String backString = reverseText(test);
+		System.out.println(backString);
 	}
-
+	public static void sum(int a, int b) {
+		int sum = a + b;
+		System.out.println("Sum is " + sum);
+	}
+	public static int sumReturn(int a, int b) {
+		int sum = a + b;
+		return sum;
+	}
+	public static void backOrder(int[] array) {
+		int length = array.length;
+		int[] reversed = new int[length];
+		for (int index = 0; index < length; index++) {
+			reversed[length - index - 1] = array[index];
+		}
+		// Print the reversed array
+		System.out.println(Arrays.toString(reversed));
+	}
+	public static String reverseText(String text) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = text.length() - 1; i >= 0; i--)
+			sb.append(text.charAt(i));
+		return sb.toString();
+	}
 }
